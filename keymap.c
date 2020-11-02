40130baece3b91662cf1b84879b0cdc494ffaa52
Kalle Macklin <kalle@delorean.se>
2020-11-02 18:36:23 +0100

    changes

--- keymap.c
+++ keymap.c
@@ -40,7 +40,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_QWERTY] = LAYOUT( \
-  KC_GRV,  KC_PGUP, KC_PGDN, _______, _______, _______, RALT(KC_Q), RALT(KC_P), RALT(KC_W), KC_PSCR, KC_PAUS, KC_DEL, \
+  KC_GRV,  KC_PGUP, KC_PGDN, KC_RALT, _______, _______, RALT(KC_Q), RALT(KC_P), RALT(KC_W), KC_PSCR, KC_PAUS, KC_DEL, \
   KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,       KC_O,       KC_P,       KC_BSPC, \
   KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,       KC_L,       KC_SCLN,    KC_QUOT, \
   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM,    KC_DOT,     KC_UP,      KC_ENT , \
